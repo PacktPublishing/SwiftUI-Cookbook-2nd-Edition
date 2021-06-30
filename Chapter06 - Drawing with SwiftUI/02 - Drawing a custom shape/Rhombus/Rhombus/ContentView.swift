3338cfb94a1c4f0051079c8405238947cd856780
giordano scalzo <giordano.scalzo@gmail.com>
2021-06-30 19:48:44 +0100

    rec 2

---
+++ Chapter06 - Drawing with SwiftUI/02 - Drawing a custom shape/Rhombus/Rhombus/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Rhombus
+//
+//  Created by Giordano Scalzo on 30/06/2021.
+//
+
+import SwiftUI
+
+struct Rhombus: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path() { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.closeSubpath()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Rhombus()
+            .fill(.orange)
+            .aspectRatio(0.7,
+                         contentMode: .fit)
+            .padding(.horizontal, 10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter06 - Drawing with SwiftUI/02 - Drawing a custom shape/Rhombus/Rhombus/RhombusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhombusApp.swift
+//  Rhombus
+//
+//  Created by Giordano Scalzo on 30/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct RhombusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
