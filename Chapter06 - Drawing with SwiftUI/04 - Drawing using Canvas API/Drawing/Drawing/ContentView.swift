0cb8b9d927f44f8f5bdd31e3cfc2e9fb703e4d56
giordano scalzo <giordano.scalzo@gmail.com>
2021-07-03 17:03:00 +0100

    Added Canvas example

---
+++ Chapter06 - Drawing with SwiftUI/04 - Drawing using Canvas API/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Giordano Scalzo on 03/07/2021.
+//
+
+import SwiftUI
+
+struct Line {
+    var points: [CGPoint]
+}
+
+struct ContentView: View {
+    @State var lines: [Line] = []
+    
+    var body: some View {
+        Canvas { ctx, size in
+            for line in lines {
+                var path = Path()
+                path.addLines(line.points)
+                
+                ctx.stroke(path, with: .color(.red),
+                           style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+            }
+        }
+        .gesture(DragGesture(minimumDistance: 0,
+                             coordinateSpace: .local)
+                    .onChanged { value in
+            let position = value.location
+            if value.translation == .zero {
+                lines.append(Line(points: [position]))
+            } else {
+                guard let lastIdx = lines.indices.last else {
+                    return
+                }
+                lines[lastIdx].points.append(position)
+            }
+        })
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter06 - Drawing with SwiftUI/04 - Drawing using Canvas API/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Giordano Scalzo on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
