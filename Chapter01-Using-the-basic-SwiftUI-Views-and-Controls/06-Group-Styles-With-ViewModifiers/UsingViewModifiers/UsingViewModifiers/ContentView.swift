7cdd046a0beb1b98cb45f72d796e1c883eb1079b
dedgar1 <jnzokwe@gmail.com>
2021-07-18 10:21:00 -0400

    ch01 r06 update

--- Chapter01-Using-the-basic-SwiftUI-Views-and-Controls/06-Group-Styles-With-ViewModifiers/UsingViewModifiers/UsingViewModifiers/ContentView.swift
+++ Chapter01-Using-the-basic-SwiftUI-Views-and-Controls/06-Group-Styles-With-ViewModifiers/UsingViewModifiers/UsingViewModifiers/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    Text("Perfect")
+        .backgroundStyle(color: Color.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundStyle: ViewModifier {
+    var bgColor: Color
+    func body(content: Content) -> some View{
+        content
+        .frame(width:UIScreen.main.bounds.width * 0.3)
+        .foregroundColor(Color.black)
+        .padding()
+        .background(bgColor)
+        .cornerRadius(CGFloat(20))
+    }
+}
+
+extension View {
+    func backgroundStyle(color: Color) -> some View{
+        self.modifier(BackgroundStyle(bgColor: color))
     }
 }
