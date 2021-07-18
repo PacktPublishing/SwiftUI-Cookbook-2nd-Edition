54ebf6c44738866bdcbfb2b319f8415eb88bbb2d
dedgar1 <jnzokwe@gmail.com>
2021-07-18 15:25:15 -0400

    ch01 r08 using sfsymbols update

---
+++ Chapter01-Using-the-basic-SwiftUI-Views-and-Controls/08-Simple graphics using-SFSymbols/UsingSFSymbols/UsingSFSymbols/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  UsingSFSymbols
+//
+//  Created by Edgar Nzokwe on 7/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wifi_on = false
+    var body: some View {
+        VStack {
+            HStack{
+                
+                Image(systemName: "c")
+                Image(systemName: "o")
+                Image(systemName: "o")
+                Image(systemName: "k")
+                
+            }.symbolVariant(.fill.circle)
+                .foregroundStyle(.yellow, .blue)
+                .font(.title)
+            HStack{
+                Image(systemName: "allergens")
+                Image(systemName: "ladybug")
+            }.symbolVariant(.fill)
+            .symbolRenderingMode(.multicolor)
+            .font(.largeTitle)
+            HStack{
+                Image(systemName: "b.circle.fill")
+                Image(systemName: "o.circle.fill")
+                    .foregroundStyle(.red)
+                Image(systemName: "o.circle.fill")
+                    .imageScale(.large)
+                Image(systemName: "k.circle.fill")
+                    .accessibility(identifier: "Letter K")
+            }.foregroundColor(.blue)
+                .font(.title)
+                .padding()
+            Toggle(isOn: $wifi_on) {
+                Label("Wifi", systemImage: "wifi")
+            }.foregroundStyle(wifi_on ? .blue : .secondary)
+                .padding()
+         
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter01-Using-the-basic-SwiftUI-Views-and-Controls/08-Simple graphics using-SFSymbols/UsingSFSymbols/UsingSFSymbols/UsingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsApp.swift
+//  UsingSFSymbols
+//
+//  Created by Edgar Nzokwe on 7/18/21.
+//
+
+import SwiftUI
+
+@main
+struct UsingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
