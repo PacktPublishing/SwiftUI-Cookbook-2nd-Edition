d48748c4d75ba1acbf569e9eb1bae2b223158416
dedgar1 <jnzokwe@gmail.com>
2021-07-25 23:04:37 -0400

    chapter 2, r1 weScoll

---
+++ Chapter02-Lists-and-ScrollViews/01-Using-ScrollViews/WeScroll/WeScroll/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  WeScroll
+//
+//  Created by Edgar Nzokwe on 7/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]
+    var body: some View {
+        VStack{
+            ScrollView {
+                ForEach(self.letters, id: \.self){ name in
+                        Image(systemName: name)
+                            .font(.largeTitle)
+                            .foregroundColor(Color.yellow)
+                            .frame(width: 50, height: 50)
+                            .background(Color.blue)
+                            .symbolVariant(.circle.fill)
+                    }
+            }
+            .frame(width:50, height:200)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack{
+                    ForEach(self.letters, id: \.self){ name in
+                        Image(systemName: name)
+                            .font(.largeTitle)
+                            .foregroundColor(Color.yellow)
+                            .frame(width: 50, height: 50)
+                            .background(Color.blue)
+                            .symbolVariant(.circle.fill)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter02-Lists-and-ScrollViews/01-Using-ScrollViews/WeScroll/WeScroll/WeScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeScrollApp.swift
+//  WeScroll
+//
+//  Created by Edgar Nzokwe on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WeScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
