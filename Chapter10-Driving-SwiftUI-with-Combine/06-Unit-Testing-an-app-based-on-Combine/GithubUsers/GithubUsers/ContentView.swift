4a3d9db95cfbd701081057b7dfcb849ee751a93e
giordano scalzo <giordano.scalzo@gmail.com>
2021-08-06 18:58:46 +0100

    fixed recipe

--- Chapter10-Driving-SwiftUI-with-Combine/06-Unit-Testing-an-app-based-on-Combine/GithubUsers/GithubUsers/ContentView.swift
+++ Chapter10-Driving-SwiftUI-with-Combine/06-Unit-Testing-an-app-based-on-Combine/GithubUsers/GithubUsers/ContentView.swift
@@ -6,11 +6,64 @@
 //
 
 import SwiftUI
+import Combine
+
+struct GithubUser: Decodable, Identifiable {
+    let id: Int
+    let login: String
+    let avatarUrl: String
+}
+
+class Github: ObservableObject {
+    @Published
+    var users: [GithubUser] = []
+    
+    private var cancellableSet: Set<AnyCancellable> = []
+    
+    func load() {
+        let url = URL(string: "https://api.github.com/users")!
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        URLSession.shared
+            .dataTaskPublisher(for: URLRequest(url: url))
+            .map(\.data)
+            .decode(type: [GithubUser].self, decoder: decoder)
+            .replaceError(with: [])
+            .receive(on: RunLoop.main)
+            .assign(to: \.users, on: self)
+            .store(in: &cancellableSet)
+    }
+}
 
 struct ContentView: View {
+    @StateObject var github = Github()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(github.users) {
+            GithubUserView(user: $0)
+        }
+        .task { github.load() }
+    }
+}
+
+struct GithubUserView: View {
+    let user: GithubUser
+    
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: user.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                Color.purple.opacity(0.1)
+            }
+            .frame(width: 40, height: 40)
+            .cornerRadius(20)
+            Spacer()
+            Text(user.login)
+        }
     }
 }
 
--- Chapter10-Driving-SwiftUI-with-Combine/06-Unit-Testing-an-app-based-on-Combine/GithubUsers/GithubUsersTests/GithubUsersTests.swift
+++ Chapter10-Driving-SwiftUI-with-Combine/06-Unit-Testing-an-app-based-on-Combine/GithubUsers/GithubUsersTests/GithubUsersTests.swift
@@ -7,27 +7,86 @@
 
 import XCTest
 @testable import GithubUsers
-
-class GithubUsersTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+import Combine
+extension XCTestCase {
+    func loadFixture(named name: String) -> Data? {
+        let bundle = Bundle(for: type(of: self))
+        let path = bundle.url(forResource: name, withExtension: "json")!
+        return try? Data(contentsOf: path)
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+final class MockURLProtocol: URLProtocol {
+    
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data?))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError("Handler is unavailable.")
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response,
+                                cacheStoragePolicy: .notAllowed)
+            if let data = data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
         }
     }
+    
+    override func stopLoading() {
+    }
+}
 
+extension XCTestCase {
+    typealias CompletionResult = (expectation: XCTestExpectation,
+        cancellable: AnyCancellable)
+    func expectValue<T: Publisher>(of publisher: T,
+                                   equalsTo closure: @escaping(T.Output) -> Bool)
+        -> CompletionResult {
+            let exp = expectation(description: "Correct values of " + String(describing: publisher))
+            let cancellable = publisher
+                .sink(receiveCompletion: { _ in },
+                      receiveValue: {
+                        if closure($0) {
+                            exp.fulfill()
+                        }
+                })
+            return (exp, cancellable)
+    }
+}
+
+class GithubUsersAppTests: XCTestCase {
+    let apiURL = URL(string: "https://api.github.com/users")!
+    func testUsersCallResult() throws {
+        // Arrange
+        URLProtocol.registerClass(MockURLProtocol.self)
+        MockURLProtocol.requestHandler = { request in
+            let response = HTTPURLResponse(url: self.apiURL, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            return (response, self.loadFixture(named: "githubUsers")!)
+        }
+        
+        let github = Github()
+        
+        let exp1 = expectValue(of: github.$users, equalsTo: { $0.first?.id ==  1 })
+        let exp2 = expectValue(of: github.$users, equalsTo: { $0.count == 30})
+        
+        // Act
+        github.load()
+        
+        // Assert
+        wait(for: [exp1.expectation, exp2.expectation], timeout: 1)
+    }
 }
