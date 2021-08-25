83825ed279feb1aef4c9154e81d494ea569520b3
giordano scalzo <giordano.scalzo@gmail.com>
2021-08-25 17:01:37 +0100

    add gitignore

---
+++ Chapter12-Handling-authentication-and-Firebase-with-SwiftUI/03-Using-Firebase-to-sign-in-using-Google/GoogleLogin/GoogleLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleLogin
+//
+//  Created by Giordano Scalzo on 25/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter12-Handling-authentication-and-Firebase-with-SwiftUI/03-Using-Firebase-to-sign-in-using-Google/GoogleLogin/GoogleLogin/GoogleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleLoginApp.swift
+//  GoogleLogin
+//
+//  Created by Giordano Scalzo on 25/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct GoogleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
