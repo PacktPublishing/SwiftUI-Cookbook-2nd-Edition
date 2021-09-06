f06f299ca56c9322f34d695b8545a3d146a4019b
dedgar1 <jnzokwe@gmail.com>
2021-09-06 11:59:30 -0400

    Ch05 r03 focus and submit

---
+++ Chapter05-Creating-new-Components-Grouping-views-in-Container-Views/03-Using-Focus-and-Submit/FocusAndSubmit/FocusAndSubmit/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  FocusAndSubmit
+//
+//  Created by Edgar Nzokwe on 9/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    enum AddressField{
+        case streetName
+        case city
+        case state
+        case zipCode
+    }
+    @State private var streetName = ""
+    @State private var city = ""
+    @State private var state = ""
+    @State private var zipCode = ""
+    
+    @FocusState private var currentFocus: AddressField?
+    var body: some View {
+        VStack{
+            TextField("Address", text: $streetName)
+                .focused($currentFocus, equals: .streetName)
+                .textContentType(.streetAddressLine1)
+                .submitLabel(.next)
+            TextField("City", text: $city)
+                .focused($currentFocus, equals: .city)
+                .textContentType(.addressCity)
+                .submitLabel(.next)
+            TextField("State", text: $state)
+                .focused($currentFocus, equals: .state)
+                .textContentType(.addressState)
+                .submitLabel(.next)
+            TextField("Zip code", text: $zipCode)
+                .focused($currentFocus, equals: .zipCode)
+                .textContentType(.postalCode)
+                .submitLabel(.done)
+        }.onSubmit {
+            switch currentFocus {
+            case .streetName:
+                currentFocus = .city
+            case .city:
+                currentFocus = .state
+            case .state:
+                currentFocus = .zipCode
+            default:
+                print("Thanks for providing your address")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter05-Creating-new-Components-Grouping-views-in-Container-Views/03-Using-Focus-and-Submit/FocusAndSubmit/FocusAndSubmit/FocusAndSubmitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAndSubmitApp.swift
+//  FocusAndSubmit
+//
+//  Created by Edgar Nzokwe on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusAndSubmitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
