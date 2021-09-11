2d69e8210b4936d5d7a3af6d07bb58bc53af20e0
dedgar1 <jnzokwe@gmail.com>
2021-09-11 18:08:11 -0400

    ch 06 r03 presentnig Confirmation Dialogs

---
+++ Chapter06-Presenting-Extra-information-to-the-user/03-Presenting-Confirmation-Dialogs/PresentingConfirmationDialog/PresentingConfirmationDialog/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  PresentingConfirmationDialog
+//
+//  Created by Edgar Nzokwe on 9/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showDialog = false
+    var title  = "Confirmation Dialog"
+    var body: some View {
+        Button("Present Confirmation Dialog"){
+                showDialog = true
+        }.confirmationDialog(title, isPresented: $showDialog){
+            Button("Dissmiss Dialog"){}
+            Button("Save"){}
+            Button("Cancel", role: .cancel){}
+            Button("Print something to console"){
+                print("Print button clicked")
+            }
+        }message: {
+            Text("""
+                Use Diaglos to give users alternatives for completing a task
+                """
+            )
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter06-Presenting-Extra-information-to-the-user/03-Presenting-Confirmation-Dialogs/PresentingConfirmationDialog/PresentingConfirmationDialog/PresentingConfirmationDialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentingConfirmationDialogApp.swift
+//  PresentingConfirmationDialog
+//
+//  Created by Edgar Nzokwe on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PresentingConfirmationDialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
