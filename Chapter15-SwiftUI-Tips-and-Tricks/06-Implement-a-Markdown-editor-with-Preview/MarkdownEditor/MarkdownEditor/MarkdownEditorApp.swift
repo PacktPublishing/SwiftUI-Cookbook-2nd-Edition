ce3ba5aace5a759e2492c8efe6194ecc3751e84f
giordano scalzo <giordano.scalzo@gmail.com>
2021-09-22 22:59:45 +0100

    Added Markdown editor

---
+++ Chapter15-SwiftUI-Tips-and-Tricks/06-Implement-a-Markdown-editor-with-Preview/MarkdownEditor/MarkdownEditor/MarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorApp.swift
+//  MarkdownEditor
+//
+//  Created by Giordano Scalzo on 22/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapter15-SwiftUI-Tips-and-Tricks/06-Implement-a-Markdown-editor-with-Preview/MarkdownEditor1/MarkdownEditorTests/MarkdownEditorTests.swift
@@ -0,0 +1,33 @@
+//
+//  MarkdownEditorTests.swift
+//  MarkdownEditorTests
+//
+//  Created by Giordano Scalzo on 18/09/2021.
+//
+
+import XCTest
+@testable import MarkdownEditor
+
+class MarkdownEditorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
