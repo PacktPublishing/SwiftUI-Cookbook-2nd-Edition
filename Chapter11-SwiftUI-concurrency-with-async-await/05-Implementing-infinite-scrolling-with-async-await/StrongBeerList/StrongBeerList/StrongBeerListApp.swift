aa7a9b62ed3c8095ad08f81bd1e563dbe44840b3
giordano scalzo <giordano.scalzo@gmail.com>
2021-09-30 22:33:05 +0100

    Add beers recipe

---
+++ Chapter11-SwiftUI-concurrency-with-async-await/05-Implementing-infinite-scrolling-with-async-await/StrongBeerList/StrongBeerList/StrongBeerListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongBeerListApp.swift
+//  StrongBeerList
+//
+//  Created by Giordano Scalzo on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct StrongBeerListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
